e47376937fc9e05344a29089d0441d7e97145e33
ali salami <asalami932@gmail.com>
2022-01-26 18:11:28 +0200

    end of lecture 282

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,10 @@
+import QuoteList from "../components/quotes/QuoteList";
+const DUMMY_QUOTES = [
+  { id: "q1", author: "ali", text: "Learning React is fun!" },
+  { id: "q2", author: "mhmd", text: "Learning React is great!" },
+];
 const AllQuotes = () => {
-    return <h1>All Quotes Page</h1>
-  };
-  
-  export default AllQuotes;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
+};
+
+export default AllQuotes;
